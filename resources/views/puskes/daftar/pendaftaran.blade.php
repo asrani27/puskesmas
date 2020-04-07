604c8526cf4a3173ff180b381080e8a951f1e27a
asranim <asrani.27@gmail.com>
2020-04-07 14:59:02 +0800

    fix

--- resources/views/puskes/daftar/pendaftaran.blade.php
+++ resources/views/puskes/daftar/pendaftaran.blade.php
@@ -131,7 +131,7 @@
                     <td><small>{{$item->pasien->kelurahan == null ? '-' : $item->pasien->kelurahan->nama}}</small></td>
                     <td><small>{{$item->asuransi == null ? '-' : $item->asuransi->nama}}</small></td>
                     <td><small>{{$item->kunjungan}}</small></td>
-                    <td><small>{{dd($item->pelayanan) == null ? 'Pendaftaran' : $item->pelayanan->statuspulang->nama}}</small></td>
+                    <td><small>{{dd($item->pelayanan->statuspulang_id) == null ? 'Pendaftaran' : $item->pelayanan->statuspulang->nama}}</small></td>
                     <td><small>{{$item->status_prb == 0 ? 'Tidak' : 'Ya'}}</small></td>
                     <td><small>{{$item->status_prolanis == 0 ? 'Tidak' : 'Ya'}}</small></td>
                     <td>
