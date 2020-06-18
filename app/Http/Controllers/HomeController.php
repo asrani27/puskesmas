7adbb43d4813a17969a390bc4bd9b652772e4413
asranim <asrani.27@gmail.com>
2020-06-18 18:37:33 +0800

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mruangan;
+use Carbon\Carbon;
+use App\Tpelayanan;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,7 +27,26 @@ class HomeController extends Controller
      */
     public function index()
     {
-        //dd(\Auth::user()->hasRole('admin') ? 'Ya' : 'Tidak');
-        return view('home');
+        if(Auth::user() == null){
+            return redirect('/');
+        }else{
+            if(Auth::user()->hasRole('superadmin')){
+                return view('home');
+            }else{
+                $ruangan = Mruangan::where('is_aktif', 'Y')->get()->map(function($item){
+                    return $item->nama;
+                });
+
+                $jml = Mruangan::where('is_aktif', 'Y')->get()->map(function($item){
+                    $month = Carbon::today()->format('m');
+                    $year = Carbon::today()->format('Y');
+
+                    $item->jml = count(Tpelayanan::where('ruangan_id', $item->id)->whereMonth('tanggal', $month)->whereYear('tanggal', $year)->get());
+                    return $item->jml;
+                });
+
+                return view('home_puskes',compact('ruangan','jml'));
+            }
+        }
     }
 }
