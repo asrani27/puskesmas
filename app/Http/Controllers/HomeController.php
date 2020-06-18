374669e31d109be47093dc3385d8e619924f418a
asranim <asrani.27@gmail.com>
2020-06-18 16:18:41 +0800

    fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,7 +29,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(Auth::user()->roles);
+        dd(Auth::user());
         if(Auth::user() == null){
             return redirect('/');
         }else{
